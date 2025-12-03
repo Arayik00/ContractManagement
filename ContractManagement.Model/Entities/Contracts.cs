bce6fe88bee3c30e85a72f74fb5e31ebe752fc5c
Arayik00 <arayik.shahnazaryan7@gmail.com>
2025-12-03 16:25:04 +0400

    validation messages for addContract page

--- ContractManagement.Model/Entities/Contracts.cs
+++ ContractManagement.Model/Entities/Contracts.cs
@@ -12,11 +12,15 @@ namespace ContractManagement.Model.Entities
         public int Id { get; set; }             // Primary key
         public int CompanyId { get; set; }      // Foreign key to Company
         public int UserId { get; set; }     // Foreign key to User
+        [Required(ErrorMessage = "The role is required")]
+        [MinLength(4, ErrorMessage = "The position must be at least 4 characters long")]
         public string Position { get; set; } = null!; // Employee role
+        [Required(ErrorMessage = "The description is required")]
+        [MinLength(10, ErrorMessage = "The description must be at least 10 characters long")]
         public string Description { get; set; } = null!;
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
-        [Range(100, double.MaxValue,ErrorMessage ="The wage must be between $200-100000")]
+        [Range(100, double.MaxValue,ErrorMessage ="The wage must be at least $100")]
         public decimal Wage { get; set; }
     }
 }
